c20eac1ec4e61861d3101f1f4b08ef26ebb68f8b
yx's VMware <itsalbertzhang@gmail.com>
2022-05-06 11:31:24 +0800

    commit by VMware

--- src/program_init/init_thread_pool.c
+++ src/program_init/init_thread_pool.c
@@ -4,17 +4,6 @@
 #include "program_init.h"
 #include "program_stat.h"
 
-// struct thread_stat_t {
-//     pthread_t *pthid;
-//     struct thread_resource_t thread_resource;
-// };
-
-// struct thread_resource_t {
-//     struct queue_t *queue; // 任务队列
-//     pthread_mutex_t mutex; // 线程锁, 加解锁后方可对队列进行操作
-//     pthread_cond_t cond;   // 子线程等待所在的条件变量
-// };
-
 int init_pthread_pool(struct thread_stat_t *thread_stat, const char *config_dir, char config[][MAX_CONFIG_LENGTH]) {
     int ret = 0;
 
