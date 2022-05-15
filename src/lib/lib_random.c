b45871519f1f958a623277f4ec48f151c3c5e56f
AliyunESC <itsalbertzhang@gmail.com>
2022-05-15 11:48:02 +0800

    commit by aliyun ECS

--- src/lib/lib_random.c
+++ src/lib/lib_random.c
@@ -5,12 +5,12 @@ static char random_gen_char(const char *alphanum, int strlen) { // Random string
 }
 
 int random_gen_str(char *str, int len, int seed) {
-    const char alphanum[] =
+    static const char alphanum[] =
         "0123456789"
         "!@#$^&*"
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         "abcdefghijklmnopqrstuvwxyz";
-    int strlen = sizeof(alphanum) - 1;
+    static const int strlen = sizeof(alphanum) - 1;
     srand(time(NULL) ^ seed);
     for (int i = 0; i < len; i++) {
         str[i] = random_gen_char(alphanum, strlen);
